693742a4a7adc6f0375666fda3267c3bdf9c7591
Shivasaireddy007 <shivasaireddy2505@gmail.com>
2025-02-18 13:40:46 +0530

    Initial project configuration setup including folder structure, environment variables, and basic config files

--- config/app.php
+++ config/app.php
@@ -221,6 +221,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        
         /*
          * Package Service Providers...
          */
