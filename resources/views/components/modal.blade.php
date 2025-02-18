070e4c48aee7ef8c8d8fa9d2baf2c8aa2567d8b6
Shivasaireddy007 <shivasaireddy2505@gmail.com>
2025-02-18 14:18:07 +0530

    Fixed password resets

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -31,6 +31,7 @@ $maxWidth = [
         nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
         prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
     }"
+    
     x-init="$watch('show', value => {
         if (value) {
             document.body.classList.add('overflow-y-hidden');
